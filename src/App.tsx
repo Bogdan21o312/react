a2f475781ed4ab4004f14f3e809caaffded54bb7
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-10 14:55:55 +0300

    Created hook useInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useInput} from "./hooks/useInput";
 
 const App = () => {
+    const userName = useInput('')
+    const password = useInput('')
+
     return (
         <div>
-          test
+          <input {...userName} type="text" placeholder="Username"/>
+          <input {...password} type="text" placeholder="password"/>
+            <button onClick={() => console.log(userName.value, password.value)}>Click</button>
         </div>
     );
 };
